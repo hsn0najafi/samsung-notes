5cac59bad9c52d58887edfd1b09e568db61c10bb
hsn8najafi <hsn8najafi@gmail.com>
2021-11-15 23:46:50 +0330

    Input For Cath Todo Text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello Baby</h1>
+                <input type="text" placeholder="Write Todo .." />
             </div>
         );
     }
