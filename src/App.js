36c81998518f21366b0bea4392522612cdd74a99
hsn8najafi <hsn8najafi@gmail.com>
2021-11-15 23:21:52 +0330

    Create State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { Component } from "react";
 
 class App extends Component {
+    state = {
+        todos: [],
+        todo: "",
+        showTodos: false,
+    };
+
     render() {
         return (
             <div>
