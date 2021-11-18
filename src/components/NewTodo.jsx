bfb6e6688d8b3bd577e09814500f5d0a6fd929cc
hsn8najafi <hsn8najafi@gmail.com>
2021-11-18 12:46:56 +0330

    Hide When Catergori Selector Opened

--- src/components/NewTodo.jsx
+++ src/components/NewTodo.jsx
@@ -1,10 +1,13 @@
 import { Fragment } from "react";
 
-const NewTodo = () => {
+const NewTodo = ({ state }) => {
     return (
         <Fragment>
             <div className="editor-container"></div>
-            <button className="new-todo-button fa fa-plus"></button>
+            <button
+                className="new-todo-button fa fa-plus animate__animated  animate__backInRight"
+                style={{ right: `${state.showCatPopUp ? "-50px" : ""} ` }}
+            ></button>
         </Fragment>
     );
 };
