4219fe8975689b5789befb8de5adab27d573afea
hsn8najafi <hsn8najafi@gmail.com>
2021-11-18 10:39:30 +0330

    Conditional Rendering Instead Of JavaScript Mouse Events

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,9 @@
 const todoEditor = document.querySelector(".editor-container");
-const popUp = document.querySelector(".popUp-catergoriSelect");
 
 window.onclick = ({ target: { classList } }) => {
     if (classList[0] == "new-todo-button") openEditor();
-    if (classList[0] == "catNameButton") openCats();
-    if (
-        classList[0] != "catNameButton" &&
-        classList[0] != "popUp-catergoriSelect"
-    )
-        closeCats();
 };
 
 const openEditor = () => {
     todoEditor.classList.toggle("editor-container__active");
-};
-
-const openCats = () => {
-    popUp.classList.add("popUp-catergoriSelect__active");
-};
-
-const closeCats = () => {
-    popUp.classList.remove("popUp-catergoriSelect__active");
 };
