540786db2353bab235740fa8d7a613a56c2eda4a
hsn8najafi <hsn8najafi@gmail.com>
2021-11-22 23:57:15 +0330

    'useContext' Instead Of 'Context.Consumer'

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,49 +1,41 @@
+import { useContext } from "react";
+
 import PopUp from "./PopUp";
 import SearchBar from "./SearchBar";
 import MoreOptions from "./MoreOptions";
 import Context from "../../context/Context";
 
 const Header = ({ handleShowSearchBar, handleShowMoreOptions }) => {
+    const c = useContext(Context);
+
     return (
-        <Context.Consumer>
-            {(c) => (
-                <div className="header-container">
-                    <div className="catName">
-                        {c.state.currentCat}
-                        <i
-                            className="catNameButton fa fa-angle-down"
-                            onClick={c.handleShowPopUp}
-                        ></i>
-                    </div>
-                    {c.state.showCatPopUp ? (
-                        <PopUp
-                            state={c.state}
-                            handleShowPopUp={c.handleShowPopUp}
-                        />
-                    ) : null}
+        <div className="header-container">
+            <div className="catName">
+                {c.state.currentCat}
+                <i
+                    className="catNameButton fa fa-angle-down"
+                    onClick={c.handleShowPopUp}
+                ></i>
+            </div>
+            {c.state.showCatPopUp ? (
+                <PopUp state={c.state} handleShowPopUp={c.handleShowPopUp} />
+            ) : null}
 
-                    <button
-                        onClick={handleShowSearchBar}
-                        className="show-searchBar"
-                    >
-                        SEARCH
-                    </button>
-                    {c.state.showSearchBar ? (
-                        <SearchBar handleShowSearchBar={handleShowSearchBar} />
-                    ) : null}
+            <button onClick={handleShowSearchBar} className="show-searchBar">
+                SEARCH
+            </button>
+            {c.state.showSearchBar ? (
+                <SearchBar handleShowSearchBar={handleShowSearchBar} />
+            ) : null}
 
-                    <button
-                        className="fa fa-ellipsis-v moreOptions-button"
-                        onClick={handleShowMoreOptions}
-                    />
-                    {c.state.showMoreOptions ? (
-                        <MoreOptions
-                            handleShowMoreOptions={handleShowMoreOptions}
-                        />
-                    ) : null}
-                </div>
-            )}
-        </Context.Consumer>
+            <button
+                className="fa fa-ellipsis-v moreOptions-button"
+                onClick={handleShowMoreOptions}
+            />
+            {c.state.showMoreOptions ? (
+                <MoreOptions handleShowMoreOptions={handleShowMoreOptions} />
+            ) : null}
+        </div>
     );
 };
 
