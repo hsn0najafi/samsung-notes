a1d42634d78c3f6d505f4a5753153444fdc3d650
hsn8najafi <hsn8najafi@gmail.com>
2021-11-29 22:59:19 +0330

    Editor Text Input

--- src/components/common/newTodo/Editor.jsx
+++ src/components/common/newTodo/Editor.jsx
@@ -1,7 +1,20 @@
 const Editor = () => {
     return (
-        <div className="editor-container animate__animated animate__slideInRight animate__faster">
-            <div></div>
+        <div className="editor-container animate__animated animate__slideInRight animate__faster flex-center-col">
+            <div className="editor-main">
+                <nav className="editor-nav"></nav>
+                <div className="new-btn">
+                    <button>
+                        <i className="fa fa-check"></i>
+                    </button>
+                </div>
+                <div className="editor-todo-title">
+                    <input type="text" />
+                </div>
+                <div className="editor-todo-text">
+                    <textarea />
+                </div>
+            </div>
         </div>
     );
 };
