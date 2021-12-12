ffbb5f05aa6ddff2f7b674d3a4dabce0c1f06f9f
hsn8najafi <hsn8najafi@gmail.com>
2021-12-12 23:39:01 +0330

    Load Data From Json File

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: 0,
+    categoriName: "work",
+    subject: "work",
+    text: "zzzzzzzzzzzzzzzzzzzzzzzzzzzccccccccccccccccccccccccccccccccccccccccccccccccccc",
+  },
+  {
+    id: 1,
+    categoriName: "default",
+    subject: "ulis quis, sem. Phasellus",
+    text: "ulis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesq",
+  },
+  {
+    id: 2,
+    categoriName: "Home",
+    subject: "ulis quis, sem",
+    text: "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+  },
+  {
+    id: 3,
+    categoriName: "default",
+    subject: "Phasellsgu",
+    text: "ulis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper",
+  },
+  {
+    id: 4,
+    categoriName: "Home",
+    subject: "gyscharbvrbt",
+    text: "ulis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper",
+  },
+];
