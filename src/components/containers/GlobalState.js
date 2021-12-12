27673edbecb025efe75c94850055581ac1c189be
hsn8najafi <hsn8najafi@gmail.com>
2021-12-12 22:54:27 +0330

    Sample Data

--- src/components/containers/GlobalState.js
+++ src/components/containers/GlobalState.js
@@ -9,7 +9,39 @@ const GlobalState = ({ children }) => {
   /**
    * All Aplication States
    */
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    {
+      id: 0,
+      categoriName: "work",
+      subject: "work",
+      text: "zzzzzzzzzzzzzzzzzzzzzzzzzzzccccccccccccccccccccccccccccccccccccccccccccccccccc",
+    },
+    {
+      id: 1,
+      categoriName: "default",
+      subject: "ulis quis, sem. Phasellus",
+      text: "ulis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesq",
+    },
+    {
+      id: 2,
+      categoriName: "Home",
+      subject: "ulis quis, sem",
+      text: "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+    },
+    {
+      id: 3,
+      categoriName: "default",
+      subject: "Phasellsgu",
+      text: "ulis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper",
+    },
+    {
+      id: 4,
+      categoriName: "Home",
+      subject: "gyscharbvrbt",
+      text: "ulis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper",
+    },
+  ]);
+
   const [todo, setTodo] = useState("");
   const [subject, setSubject] = useState("");
   const [categories, setCategories] = useState([
